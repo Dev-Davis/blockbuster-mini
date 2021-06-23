54740bd1b7eee260beb7f4dea2fb5737d52b1b0f
dev_davis <sean.dev.davis@gmail.com>
2021-06-23 03:16:20 -0500

    made navbar a little cleaner

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,42 @@
 import React from "react";
 
+import "./Navbar.css";
+
 function Navbar() {
-  return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
-  );
+  const memberMenu = () => {
+    return (
+      <div className="ui container menu">
+        <div className="secondary item">
+          <a href="/">
+            <i className="barcode icon"></i>
+          </a>
+        </div>
+        <div className="item">
+          <a href="/profile">Profile</a>
+        </div>
+        <div className="item">
+          <a href="/movies">Movies</a>
+        </div>
+        <div className="item">
+          <a href="/games">Games</a>
+        </div>
+        <div className="right item">
+          <a href="/">Sign Out</a>
+        </div>
+      </div>
+    );
+  };
+
+  // const guestMenu = () => {
+  //   return (
+  //     <div className="ui menu">
+  //       <div className="item">Blockbuster Mini</div>
+  //       <div className="right item">Sign Out</div>
+  //     </div>
+  //   );
+  // };
+
+  return <div className="nav">{memberMenu()}</div>;
 }
 
 export default Navbar;
