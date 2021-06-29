fbfcf7b59fe63950bb13de3268c6c613a51f9cd8
dev_davis <sean.dev.davis@gmail.com>
2021-06-29 02:56:36 -0500

    got the games filtering

--- src/Components/Games/games.js
+++ src/Components/Games/games.js
@@ -6,6 +6,24 @@ import axios from "axios";
 
 function Games() {
   const [games, setGames] = useState([]);
+  const [filter, setFilter] = useState([]);
+  const [selectedPlatform, setPlatform] = useState("all");
+
+  const filterGames = (e, filterType) => {
+    const newPlatform = e.target.value;
+
+    switch (filterType) {
+      case "platform":
+        setPlatform(newPlatform);
+        const selectedPlatform = games.filter(
+          (game) => game.platform === newPlatform
+        );
+        setFilter(selectedPlatform);
+        break;
+      default:
+        break;
+    }
+  };
 
   useEffect(() => {
     axios
@@ -13,44 +31,34 @@ function Games() {
       .then((res) => {
         const games = Object.values(res.data);
         setGames(games);
+        setFilter(games);
       })
       .catch((err) => console.log("could not get games", err));
   }, []);
-
-  const filterGames = (e) => {
-    const buttonId = e.target.id; // id of button is game platform
-    console.log(buttonId)
-    games.filter((game) => buttonId === game.platform);
-  };
 
   return (
     <div>
       <h2 className="text-center">Games</h2>
       <div className="ui container text-center">
         <div className="row">
-          <div className="game-selection-buttons">
-            <button className="ui button" id="all-games" onClick={filterGames}>
-              All Games
-            </button>
-            <button
-              className="ui button"
-              id="Playstation"
-              onClick={filterGames}
+          <div className="ui container text-center">
+            <select
+              name="platform"
+              id="platform"
+              onChange={(e) => filterGames(e, "platform")}
+              className="ui multiple selection dropdown"
             >
-              PlayStation
-            </button>
-            <button className="ui button" id="Nintendo64" onClick={filterGames}>
-              Nintendo 64
-            </button>
-            <button className="ui button" id="Dreamcast" onClick={filterGames}>
-              Dreamcast
-            </button>
+              <option value="all">All Movies (Select a genre)</option>
+              <option value="Dreamcast">Dreamcast</option>
+              <option value="Nintendo64">Nintendo 64</option>
+              <option value="Playstation">Playstation</option>
+            </select>
           </div>
         </div>
       </div>
       <div className="container-fluid">
         <div className="row">
-          {games.map((game) => (
+          {filter.map((game) => (
             <GameDisplay key={game.id} game={game} />
           ))}
         </div>
